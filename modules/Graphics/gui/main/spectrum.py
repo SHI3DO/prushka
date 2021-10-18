3f6a8972a0d0df951f9ea9572104e460a4f6b7dc
Shio <shio7113@gmail.com>
2021-10-18 17:11:59 +0900

    sss

--- modules/Graphics/gui/main/spectrum.py
+++ modules/Graphics/gui/main/spectrum.py
@@ -1,2 +1,61 @@
-def spectrum(pg, screen, screensize):
-    print("spectrum")
+from modules.Sounds.audioanalyzer import *
+
+
+def spectrum(pg, screen, screensize, gettickslastframe, timecount, bars, analyzer, min_radius, max_radius, radius,
+             min_decibel, max_decibel, bass_trigger, bass_trigger_started):
+    avg_bass = 0
+    poly = []
+
+    t = pg.time.get_ticks()
+    deltaTime = (t - gettickslastframe) / 1000.0
+    gettickslastframe = t
+
+    timecount += deltaTime
+
+    for b1 in bars:
+        for b in b1:
+            b.update_all(deltaTime, pygame.mixer.music.get_pos() / 1000.0, analyzer)
+
+    for b in bars[0]:
+        avg_bass += b.avg
+
+    avg_bass /= len(bars[0])
+
+    if avg_bass > bass_trigger:
+        if bass_trigger_started == 0:
+            bass_trigger_started = pygame.time.get_ticks()
+        if (pygame.time.get_ticks() - bass_trigger_started) / 1000.0 > 2:
+            bass_trigger_started = 0
+
+        newr = min_radius + int(
+            avg_bass * ((max_radius - min_radius) / (max_decibel - min_decibel)) + (max_radius - min_radius))
+        radius_vel = (newr - radius) / 0.15
+
+    elif radius > min_radius:
+        bass_trigger_started = 0
+        polygon_bass_color = None
+        radius_vel = (min_radius - radius) / 0.15
+
+    else:
+        bass_trigger_started = 0
+        poly_color = (255, 255, 255)
+        polygon_bass_color = None
+        polygon_color_vel = [0, 0, 0]
+
+        radius_vel = 0
+        radius = min_radius
+
+    radius += radius_vel * deltaTime
+
+    for b1 in bars:
+        for b in b1:
+            b.x, b.y = int(screensize.current_w / 2) + radius * math.cos(math.radians(b.angle - 90)), \
+                       int(screensize.current_h / 2) + radius * math.sin(
+                           math.radians(b.angle - 90))
+            b.update_rect()
+
+            poly.append(b.rect.points[3])
+            poly.append(b.rect.points[2])
+
+    pg.draw.polygon(screen, (0, 0, 0), poly)
+    pg.draw.circle(screen, (255, 255, 255), (int(screensize.current_w / 2), int(screensize.current_h / 2)), int(radius))
--- prushka.py
+++ prushka.py
@@ -5,6 +5,7 @@ from modules.Presence.RPC import discordrpc
 from modules.Control import clickhandler
 from modules.Graphics.gui import background
 from modules.Graphics.gui import mainscene
+from modules.Sounds.audioanalyzer import *
 
 pg.init()
 icon = pg.image.load("./resources/prushka/icon32.ico")
@@ -25,6 +26,60 @@ f.close()
 f = open("./resources/tmp/options_screen.txt", 'w', encoding='UTF-8')
 f.write("0")
 f.close()
+
+
+audiot = pg.time.get_ticks()
+audiotimecount = 0
+audiobars = []
+audiotmp_bars = []
+min_radius = 100
+max_radius = 150
+min_decibel = -80
+max_decibel = 80
+radius = min_radius
+bass_trigger = -30
+bass_trigger_started = 0
+bass = {"start": 50, "stop": 100, "count": 12}
+heavy_area = {"start": 120, "stop": 250, "count": 40}
+low_mids = {"start": 251, "stop": 2000, "count": 50}
+high_mids = {"start": 2001, "stop": 6000, "count": 20}
+freq_groups = [bass, heavy_area, low_mids, high_mids]
+
+length = 0
+
+for group in freq_groups:
+    g = []
+    s = group["stop"] - group["start"]
+    count = group["count"]
+    reminder = s%count
+    step = int(s/count)
+    rng = group["start"]
+    for i in range(count):
+        arr = None
+        if reminder > 0:
+            reminder -= 1
+            arr = np.arange(start=rng, stop=rng + step + 2)
+            rng += step + 3
+        else:
+            arr = np.arange(start=rng, stop=rng + step + 1)
+            rng += step + 2
+        g.append(arr)
+        length += 1
+    audiotmp_bars.append(g)
+
+angle_dt = 360/length
+ang = 0
+for g in audiotmp_bars:
+    gr = []
+    for c in g:
+        gr.append(
+            RotatedAverageAudioBar(int(screensize.current_w)+radius*math.cos(math.radians(ang - 90)),
+                                   int(screensize.current_h)+radius*math.sin(math.radians(ang - 90)),
+                                   c, (255, 0, 255), angle=ang, width=8, max_height=370))
+        ang += angle_dt
+
+
+analyzer = AudioAnalyzer()
 
 
 def bgm_play():
@@ -67,7 +122,8 @@ while mainLoop:
 
     # MainScene
     mainscene.play(pg, screen, screensize, clock, LightFont, RegularFont, play_img, pause_img, stop_img, next_img,
-                   prev_img, gear_img)
+                   prev_img, gear_img, audiot, audiotimecount, audiobars, analyzer, min_radius, max_radius, radius,
+                   min_decibel, max_decibel, bass_trigger, bass_trigger_started)
 
     dt = clock.tick(120)
     pg.display.update()
