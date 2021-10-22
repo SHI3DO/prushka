c8ec0cb3a79c083661e9d970f229f8ee45d2cff4
Shio <shio7113@gmail.com>
2021-10-22 12:44:44 +0900

    active

--- modules/utils/reqdir.py
+++ modules/utils/reqdir.py
@@ -13,3 +13,7 @@ def make():
     f = open("./resources/runtime/music_pnum.txt", 'w', encoding='UTF-8')
     f.write("0")
     f.close()
+
+    f = open("./resources/runtime/mainscene_active.txt", 'w', encoding='UTF-8')
+    f.write("0")
+    f.close()
